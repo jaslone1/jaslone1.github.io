2bfbce7f7209fee8840a5a2863a64a841d60d18b
jaslone <jaredmslone@gmail.com>
2020-09-02 12:42:09 -0500

    progress

--- app.js
+++ app.js
@@ -3,32 +3,36 @@ $(() => {
       event.preventDefault();
       const userInput = $('input[type="text"]').val()
 
-    // $.ajax({
-  	//    url: 'https://www.theaudiodb.com/api/v1/json/1/search.php?s=' + userInput
-    // }).then(
-    //   (data)=>{
-    //
-    //     $('.title').html(data.artists[0].strArtist);
-    //     $('.bio').html(data.artists[0].strBiographyEN);
-    //     $('.banner').attr('src', data.artists[0].strArtistBanner);
-    //     $('#estab').html("est. " + data.artists[0].intFormedYear);
-    //     $('.pic1').attr('src', data.artists[0].strArtistFanart);
-    //     $('.pic2').attr('src', data.artists[0].strArtistFanart2);
-    //
-    //     $('#buttonPic1').on('click', () => {
-    //       $('.albumArt').children().hide();
-    //       $('.pic1').show();
-    //     })
-    //     $('#buttonPic2').on('click', () => {
-    //       $('.albumArt').children().hide();
-    //       $('.pic2').show();
-    //     })
-    //     $('#bio').on('click', () => {
-    //       $('.albumArt').children().hide();
-    //       $('.bio').show();
-    //     })
-    //     console.log(data);
-    //   })
+    $.ajax({
+  	   url: 'https://www.theaudiodb.com/api/v1/json/1/search.php?s=' + userInput
+    }).then(
+      (data)=>{
+
+        $('.title').html(data.artists[0].strArtist);
+        $('.bio').html(data.artists[0].strBiographyEN);
+        $('.banner').attr('src', data.artists[0].strArtistBanner);
+        $('#estab').html("est. " + data.artists[0].intFormedYear);
+        $('.pic1').attr('src', data.artists[0].strArtistFanart);
+        $('.pic2').attr('src', data.artists[0].strArtistFanart2);
+
+        let currentIndex = 0;
+        const numOfImages = $('.albumArt').children().length - 1;
+
+        const nextBtn = '<input type="button" value="next" id="nextBTN"/>';
+        $next = $('<div>')
+        $('.carousel').append($next)
+        $($next).append(nextBtn)
+          $('#nextBTN').on('click', () => {
+          $('.albumArt').children().eq(currentIndex).hide();
+          if (currentIndex < numOfImages) {
+            currentIndex++;
+          } else {
+            currentIndex = 0
+          }
+            $('.albumArt').children().eq(currentIndex).show();
+          })
+        console.log(data);
+      })
     $.ajax({
       url: 'https://www.theaudiodb.com/api/v1/json/1/searchalbum.php?s=' + userInput
     }).then(
--- second.js
+++ second.js
@@ -1,7 +1,14 @@
 $(() => {
-  $('form').on('submit', (event)=>{
+  $('form').on('submit', event=>{
       event.preventDefault();
+
+
+
+      $('.header').hide();
       const userInput = $('input[type="text"]').val()
+
+
+
     $.ajax({
   	   url: 'https://www.theaudiodb.com/api/v1/json/1/search.php?s=' + userInput
     }).then(
@@ -14,26 +21,46 @@ $(() => {
         $('.pic1').attr('src', data.artists[0].strArtistFanart);
         $('.pic2').attr('src', data.artists[0].strArtistFanart2);
 
-        $('#buttonPic1').on('click', () => {
-          $('.albumArt').children().hide();
-          $('.pic1').show();
-        })
-        $('#buttonPic2').on('click', () => {
-          $('.albumArt').children().hide();
-          $('.pic2').show();
-        })
-        $('#bio').on('click', () => {
-          $('.albumArt').children().hide();
-          $('.bio').show();
-        })
+        let currentIndex = 0;
+        const numOfImages = $('.albumArt').children().length - 1;
+
+        const nextBtn = '<input type="button" value="next" id="nextBTN"/>';
+        $next = $('<div>')
+        $('.carousel').append($next)
+        $($next).append(nextBtn)
+          $('#nextBTN').on('click', () => {
+          $('.albumArt').children().eq(currentIndex).hide();
+          if (currentIndex < numOfImages) {
+            currentIndex++;
+          } else {
+            currentIndex = 0
+          }
+            $('.albumArt').children().eq(currentIndex).show();
+          })
+
         console.log(data);
+      $.ajax({
+        url: 'https://www.theaudiodb.com/api/v1/json/1/searchalbum.php?s=' + userInput
+      }).then(
+        (data)=>{
 
+          const discoBtn = '<input type="button" value="discography" id="discoBTN"/>';
+          $disco = $('<div>')
+          $('body').append($disco)
+          $($disco).append(discoBtn)
+          console.log(discoBtn);
+          $('#discoBTN').on('click', () => {
 
+            var discography = (data.album.length)
+
+            for (var i = 0; i < data.album.length; i++) {
+                console.log(data.album[i].strAlbum);
+                $('#list').append().text(data.album[i].strAlbum)
+
+          }
       })
-
-    //   () => {
-    //   console.log('badrequest');
     }
     );
-
   })
+  })
+})
